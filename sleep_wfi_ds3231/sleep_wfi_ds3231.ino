1589bf55a16c393f38f7b997cdeaf8f7928b8063
Don Blair <donblair@gmail.com>
2017-08-28 21:41:25 -0400

    update

--- sleep_wfi_ds3231/sleep_wfi_ds3231.ino
+++ sleep_wfi_ds3231/sleep_wfi_ds3231.ino
@@ -6,8 +6,6 @@
 
 #define interruptPin 6
 
-int ALARM_TRIGGERED=0;
-
 DS3231 rtc; //Create the DS3231 object
 static uint8_t prevSecond=0; 
 const int interval_sec = 5; //interval in seconds
@@ -22,12 +20,8 @@ void setup ()
 	 Wire.begin();
 	
 	 rtc.begin(); 
-   updateAlarm(interval_sec); // for some reason this is necessary here?
-   sleep_prep();
-   
    
 }
-
 
 
 void loop () 
@@ -36,19 +30,15 @@ void loop ()
 	for (int i=0;i<3;i++) { // indicate visually
     Blink(LED_BUILTIN,1000);
    }
-
-   updateAlarm(interval_sec);
    
-   //__WFI();  // wake from interrupt
-
-   fall_asleep();
+   fall_asleep(interval_sec);
 
    for (int i=0;i<3;i++) { // indicate visually
     Blink(LED_BUILTIN,100);
    }
+   delay(1000);
    Serial.println(" External Interrupt detected "); //indicate on serial port
 
-   delay(10);
 } 
 
   
@@ -57,7 +47,6 @@ void EIC_ISR()
 {
   //Keep this as short as possible. Possibly avoid using function calls
   detachInterrupt(digitalPinToInterrupt(interruptPin));
-   ALARM_TRIGGERED=1;
   
 }
 
@@ -71,25 +60,12 @@ void Blink(byte PIN, int DELAY_MS) {
 }
 
 
-void updateAlarm (int interval_sec) {
+void fall_asleep (int interval_sec) {
 
   rtc.clearAlarm(); //resets the alarm interrupt status on the RTC
   attachInterrupt(digitalPinToInterrupt(interruptPin), EIC_ISR, FALLING);  // Attach interrupt to pin 6 with an ISR and when the pin state CHANGEs
-  DateTime now = rtc.now(); //get the current time
-  DateTime nextAlarm = DateTime(now.unixtime() + interval_sec);
-  rtc.enableAlarm(nextAlarm); //Sets the alarm on the RTC to the specified time (using the DateTime Object passed in)
-  delay(10); //wait for a moment for everything to complete
-  ALARM_TRIGGERED=0;
-}
-
-void fall_asleep(){
-
-  __WFI();
-  
-}
-void sleep_prep() {
-
-SYSCTRL->XOSC32K.reg |=  (SYSCTRL_XOSC32K_RUNSTDBY | SYSCTRL_XOSC32K_ONDEMAND); // set external 32k oscillator to run when idle or sleep mode is chosen
+ 
+  SYSCTRL->XOSC32K.reg |=  (SYSCTRL_XOSC32K_RUNSTDBY | SYSCTRL_XOSC32K_ONDEMAND); // set external 32k oscillator to run when idle or sleep mode is chosen
   REG_GCLK_CLKCTRL  |= GCLK_CLKCTRL_ID(GCM_EIC) |  // generic clock multiplexer id for the external interrupt controller
                        GCLK_CLKCTRL_GEN_GCLK1 |  // generic clock 1 which is xosc32k
                        GCLK_CLKCTRL_CLKEN;       // enable it
@@ -105,6 +81,11 @@ SYSCTRL->XOSC32K.reg |=  (SYSCTRL_XOSC32K_RUNSTDBY | SYSCTRL_XOSC32K_ONDEMAND); 
   // It is either Idle mode or Standby mode, not both. 
   SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;   // Enable Standby or "deep sleep" mode
   
+  
+  DateTime now = rtc.now(); //get the current time
+  DateTime nextAlarm = DateTime(now.unixtime() + interval_sec);
+  rtc.enableAlarm(nextAlarm); //Sets the alarm on the RTC to the specified time (using the DateTime Object passed in)
+  delay(10); //wait for a moment for everything to complete
+  __WFI();
 }
-
 
